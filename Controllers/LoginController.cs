3655ef9cfedb20cef04d2902a4d6bf376fe9d0b2
user <user@LAPTOP-K1LVTM6V>
2022-09-02 12:48:55 +0800

    加入專案檔案。

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
+using System;
+
+namespace WebApplication2.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult ProcessLogin(UserModel usermodel)
+        {
+            if (usermodel.UserName == "" && usermodel.Password == "")
+            {
+                return View("LoginSuccess", usermodel);
+            }
+            else
+            {
+                return View("LoginFailure", usermodel);
+            }
+        }
+    }
+}
